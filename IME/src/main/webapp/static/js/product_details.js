943f23542fafd5e164adf89df858b16fccd09493
Sisu <CrazyLai1996@outlook.com>
2018-03-11 19:39:39 +0800

    产品详情静态页面，完成使用心得分析块

--- IME/src/main/webapp/static/js/product_details.js
+++ IME/src/main/webapp/static/js/product_details.js
@@ -183,5 +183,8 @@ $(document).ready(function() {
     var powerWidth = parseInt($("."+powerClassName).css("max-width"));
     $(power).width(powerWidth * parseFloat($(power).data("power")));
   }
+  //评分占比
   showPower($(".rating-power"));
+  //评论心得分析
+  showPower($(".item-power"));
 });
