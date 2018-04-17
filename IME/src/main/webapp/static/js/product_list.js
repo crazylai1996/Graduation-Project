48f8d9338e345a1b63829b9fef28bd8ff51db626
Sisu-5175 <CrazyLai1996@outlook.com>
2018-04-17 21:15:27 +0800

    更新产品列表静态页面

--- IME/src/main/webapp/static/js/product_list.js
+++ IME/src/main/webapp/static/js/product_list.js
@@ -1,3 +1,6 @@
+/**
+ * [产品更多属性]
+ */
 $(".property-more-btn").click(function(){
   if(!$(this).hasClass("pointer-top")){
     $(this).parent().siblings(".property-items").css("height","auto");
@@ -6,3 +9,10 @@ $(".property-more-btn").click(function(){
   }
   $(this).toggleClass("pointer-top");
 });
+/**
+ * [加载评分插件]
+ */
+var $input = $('input.rating'), count = Object.keys($input).length;
+if (count > 0) {
+    $input.rating();
+}
