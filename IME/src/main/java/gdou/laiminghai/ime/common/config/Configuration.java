5906833fab584ae363defae8914a185bae432859
Sisu-5175 <CrazyLai1996@outlook.com>
2018-04-27 19:28:17 +0800

    应用配置

---
+++ IME/src/main/java/gdou/laiminghai/ime/common/config/Configuration.java
@@ -0,0 +1,16 @@
+package gdou.laiminghai.ime.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Configuration {
+	
+//	public static String DARASOURCE; // 数据源
+//	
+//	@Value("${datasource}") 
+//	public void setDataSource(String dataSource) {
+//		Configuration.DARASOURCE = dataSource;
+//	}
+//	
+}
